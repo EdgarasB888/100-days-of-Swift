de40e2d813fdada3e2fb735b634a811359382e12
EdgarasB888 <edgaras0071@gmail.com>
2022-11-28 13:23:14 +0200

    Challenge finished

--- Day 24, 25 (project 4)/Project4/Project4/Controller/ViewController.swift
+++ Day 24, 25 (project 4)/Project4/Project4/Controller/ViewController.swift
@@ -29,12 +29,14 @@ class ViewController: UIViewController, WKNavigationDelegate
         
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .plain, target: webView, action: #selector(webView.goBack))
+        let forwardButton = UIBarButtonItem(image: UIImage(systemName: "arrow.right"), style: .plain, target: webView, action: #selector(webView.goForward))
+                
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
-        
-        toolbarItems = [progressButton, spacer, refresh]
+    
+        toolbarItems = [progressButton, spacer, backButton, forwardButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
@@ -101,6 +103,22 @@ class ViewController: UIViewController, WKNavigationDelegate
             }
         }
         decisionHandler(.cancel)
+        actionController(title: url!)
+    }
+    
+    func actionController(title: URL)
+    {
+        if !title.absoluteString.contains("about")
+        {
+            let ac = UIAlertController(title: "Warning!", message: "The website \(title) is not allowed", preferredStyle: .actionSheet)
+            ac.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            
+            present(ac, animated: true)
+        }
+        else
+        {
+            print("about")
+        }
     }
 }
 
