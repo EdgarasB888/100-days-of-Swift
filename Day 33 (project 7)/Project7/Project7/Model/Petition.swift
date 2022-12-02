a22de5209072a945ba3ea5638d2a681ba2e26a2a
EdgarasB888 <edgaras0071@gmail.com>
2022-12-02 15:06:15 +0200

    Project7 finished (challenge in progress)

---
+++ Day 33 (project 7)/Project7/Project7/Model/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Edgaras Blauzdys on 2022-12-02.
+//
+
+import Foundation
+
+struct Petition: Codable
+{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
